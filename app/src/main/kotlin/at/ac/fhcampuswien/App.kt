8f3c86d267fc9945873ed66616b01d91de318924
LukasBrezina <lukas.brezina@stud.fh-campuswien.ac.at>
2024-02-19 00:38:23 +0100

    exercise 1 done
    - reworked the generatingRandom... method -> now working with an other random module ... i think i used the wrong one before, now it works
    
    - more clean code written -> edited a few comments, improved code structure, removed unneccesary arguments

--- app/src/main/kotlin/at/ac/fhcampuswien/App.kt
+++ app/src/main/kotlin/at/ac/fhcampuswien/App.kt
@@ -3,34 +3,31 @@
  */
 package at.ac.fhcampuswien
 
-import kotlin.random.Random
-import kotlin.system.exitProcess
-
 class App {
-    // Game logic for a number guessing game
     fun playNumberGame(digitsToGuess: Int = 4) {
         //TODO: build a menu which calls the functions and works with the return values
+
         println("LET THE GAMES BEGIN")
         println("You have unlimited guesses, so take your time and use your brain...")
-        println("Enter your first guess, you will receive feedback in a form of -> 2:1 for example" +
+        println("Enter your first guess, you will receive feedback in a form of -> 2:1 for example " +
                 "which stands for 2 right digits (not knowing if they are on the right position) and 1 " +
                 "right digit which is also on the right position!")
+        println("The number to guess contains " + digitsToGuess + " digits")
+
         try {
             // throw IllegalArgumentException when too many or too less digits given
             if (digitsToGuess<1 || digitsToGuess>9) {
                 throw IllegalArgumentException("Too many or too less digits")
             }
-            // create random number using generateRandomNonRepeatingNumber method
+
             val number = generateRandomNonRepeatingNumber(digitsToGuess)
             var printed = 0
 
-            // testing: println(number)
-
             // playing the game until generated number is the same as the input
             while (printed.toString() != number.toString()) {
-                // read in input -> readLine or Null -> "!!" means value will never be null
+                //"!!" means value will never be null
                 printed = readlnOrNull()!!.toInt()
-                // catching when guessed number and generated number do not have the same length
+
                 if (printed.toString().length != number.toString().length) {
                     println("Input does not have the same length as the given random number: please try " +
                             "again with a number with the length: " + number.toString().length)
@@ -42,7 +39,6 @@ class App {
 
         } catch (e: Exception) {
             println("Something went wrong... Try again")
-            exitProcess(0)
         }
     }
 
@@ -61,18 +57,17 @@ class App {
      * @throws IllegalArgumentException if the length is more than 9 or less than 1.
      */
     val generateRandomNonRepeatingNumber: (Int) -> Int = { length ->
-        // set random
-        val random = Random(System.currentTimeMillis())
-        // create lists, one with digits from 0-9 and an empty one (where we add the values to)
-        val digits = (0..9).toMutableList()
-        val result = mutableListOf<Int>()
-        // iteration from 1 to the given length of the number
-        for (i in 1..length) {
-            // generate random from 0 to the size of digits
-            val index = random.nextInt(0,digits.size)
-            // add the value on this index to the result and remove it from the digits list -> so there are no duplicates
-            result.add(digits[index])
-            digits.removeAt(index)
+        val result = mutableListOf<String>()
+
+        if (length >= 1 && length <= 9) {
+            while (result.size < length) {
+                val random = (0..9).random().toString()
+                if (!(random in result)) {
+                    result.add(random)
+                }
+            }
+        } else {
+            throw IllegalArgumentException("Too long or too short")
         }
         result.joinToString("").toInt()
     }
@@ -96,43 +91,32 @@ class App {
     val checkUserInputAgainstGeneratedNumber: (Int, Int) -> CompareResult = { input, generatedNumber ->
         //TODO implement the function
 
-        // counting variables
+        // variables
         var rightDigits = 0
         var completedDigits = 0
-        // list containing characters which appear more often -> so the rightDigits count is correct
-        val alreadyAdded = mutableListOf<Char>()
-
         var inputString = input.toString()
         val generatedNumberString = generatedNumber.toString()
-        /* testing
-        println(inputString)
-        println(generatedNumberString)
-        */
+
+        // list containing seen characters
+        val alreadyAdded = mutableListOf<Char>()
+
         try {
             // catching if the length of input and generated number is not the same
             if (inputString.length != generatedNumberString.length) {
                 throw IllegalArgumentException("Input and generated Number not same length!")
             }
             // increasing completedDigits and rightDigits in case of guessing the correct number + position
-            // iterating through each character (=indices) of the generatedNumberString
             for (i in generatedNumberString.indices) {
                 if (inputString[i] == generatedNumberString[i]) {
                     completedDigits++
-                    // println(alreadyAdded)
-                    if (inputString[i] !in alreadyAdded) {
-                        rightDigits++
-                    }
-                    alreadyAdded.add(inputString[i])
-                } else if (inputString[i] in generatedNumberString) {
-                    if (inputString[i] !in alreadyAdded) {
-                        rightDigits++
-                    }
+                }
+                if (inputString[i] in generatedNumberString && !(inputString[i] in alreadyAdded)) {
+                    rightDigits++
                     alreadyAdded.add(inputString[i])
                 }
             }
-        // catching exception
         } catch (e: Exception) {
-            throw IllegalArgumentException("Input and generated Number do not have the same length!")
+            throw IllegalArgumentException("Oops.. something went wrong..")
         }
         CompareResult(rightDigits, completedDigits)
     }
